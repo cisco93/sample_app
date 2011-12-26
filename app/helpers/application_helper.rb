3a860ee0e3072a972b7505fe033da6ae3b6a3099
Francisco Machado <mfrancisco1993@aol.com>
2011-12-26 17:24:54 +0100

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title
+      "#{base_title} | #{@title}"
+    else 
+      base_title
+    end
+  end
+  
+  
 end
